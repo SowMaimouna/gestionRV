7f114681e1d1f9cb7966f0c3ecef7d6dbec64847
SowMaimouna <sowmounass55@gmail.com>
2025-07-05 19:47:19 +0000

    Ajout de la gestion d'email reçu par les patients lors de la confirmation de leur rendez-vous

--- testEmail.js
+++
@@ -1,5 +0,0 @@
-const sendEmail = require('./utils/email');
-
-sendEmail('sowmounass55@gmail.com', 'Test Email', 'Ceci est un test.')
-  .then(() => console.log('✅ Email envoyé'))
-  .catch((err) => console.error('❌ Erreur:', err));
